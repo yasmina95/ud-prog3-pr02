93dd27ecfd28c44d3eaadd60cc72a2b850da6194
Yasmin <yasmin.martin@opendeusto.es>
2016-10-18 18:48:04 +0200

    Cambio de nombre y paso 4

---
+++ Prog3p2.0/src/ud/prog3/pr02/CocheTest.java
@@ -0,0 +1,14 @@
+package ud.prog3.pr02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CocheTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
